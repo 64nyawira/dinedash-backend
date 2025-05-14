dfe24bcb995fe11229ac38019971c6f33f24d288
54nyawira <charitynyawirairungu@gmail.com>
2025-05-14 16:02:14 +0300

    updated the cors to the live frontend path

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: 'singular-daffodil-cc2224.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
